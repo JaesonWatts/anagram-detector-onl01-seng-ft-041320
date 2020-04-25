c4e79a17e997f1c3706c18f1c66e25f2997d855a
Jaeson Watts <jaeson.watts@gmail.com>
2020-04-25 05:18:56 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,6 +1,6 @@
 # Your code goes here!
 
-def Anagram
+class Anagram
   
   attr_accessor :word
   
@@ -10,14 +10,9 @@ def Anagram
   
   def match(word_array)
     word_array.select do |element|
-      @word.split("").sort == element.split("".sort)  
+      word.split("").sort == @word.split("".sort)  
     end
   end
 
 
 end
-
-
-listen = Anagram.new("listen")
-
-listen.match(%w(enlists google inlets banana))
