cea49de306df8f65a9115d5e0d4efcaeef7a78dd
Jaeson Watts <jaeson.watts@gmail.com>
2020-04-25 05:19:37 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,9 +4,9 @@ class Anagram
   
   attr_accessor :word
   
-  # def intialize(word)
-  #   @word = word
-  # end
+  def intialize(word)
+    @word = word
+  end
   
   def match(word_array)
     word_array.select do |element|
