fd0fc6e6d9233d60835b01f13b80e319caca5b9e
Jaeson Watts <jaeson.watts@gmail.com>
2020-04-25 05:16:58 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+
+def Anagram
+  
+  attr_accessor :word
+  
+  def intialize(word)
+    @word = word
+  end
+  
+  def match(word_array)
+    word_array.select do |word|
+      word.split("").sort == @word.split("".sort)  
+    end
+  end
+
+
+end
